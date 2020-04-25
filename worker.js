5d1b511d2c5450fb0cf5a68196af125e9684a29d
jyk <1684502482@qq.com>
2020-04-26 03:12:10 +0800

    Site updated: 2020-04-26 03:12:08

--- worker.js
+++ worker.js
@@ -1,6 +1,13 @@
-self.onmessage = e => {
+self.onmessage = async e => {
   if (e.data) {
-    // count2aili
-    fetch(`https://1798248453614055.cn-shanghai.fc.aliyuncs.com/2016-08-15/proxy/getBdy-GreetingService-EE494AFDB77E/wmask/`, { method: 'post', body: e.data })
+    const countUrl = 'https://1798248453614055.cn-shanghai.fc.aliyuncs.com/2016-08-15/proxy/getBdy-GreetingService-EE494AFDB77E/count-wmask/';
+    const countUrl = 'http://localhost:8000/2016-08-15/proxy/getBdy-GreetingService-EE494AFDB77E/count-wmask';
+    await fetch(countUrl, {
+      headers: {
+        "content-type": "application/json; charset=UTF-8",
+      },
+      method: 'post',
+      body: e.data,
+    })
   }
 }
