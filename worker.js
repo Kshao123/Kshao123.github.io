957554bb2548f4fe43ca50432684c4b739eb35d0
jyk <1684502482@qq.com>
2020-05-03 20:48:29 +0800

    Site updated: 2020-05-03 20:48:27

--- worker.js
+++ worker.js
@@ -1,7 +1,7 @@
 self.onmessage = async e => {
   if (e.data) {
     const countUrl = 'https://1798248453614055.cn-shanghai.fc.aliyuncs.com/2016-08-15/proxy/getBdy-GreetingService-EE494AFDB77E/count-wmask/';
-    const countUrl = 'http://localhost:8000/2016-08-15/proxy/getBdy-GreetingService-EE494AFDB77E/count-wmask';
+    // const countUrl = 'http://localhost:8000/2016-08-15/proxy/getBdy-GreetingService-EE494AFDB77E/count-wmask';
     await fetch(countUrl, {
       headers: {
         "content-type": "application/json; charset=UTF-8",
